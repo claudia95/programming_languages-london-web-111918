40cf68ac7281340849bd4b3bf09cc60a5c466ebb
Claudia Fox <claudiafox95@icloud.com>
2018-11-14 23:16:47 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -6,7 +6,7 @@ def reformat_languages(languages)
         new_hash[language][:style] << style
       else
         new_hash[language] = type
-        new_hash[language][:style] << [style]
+        new_hash[language][:style] = [style]
       end
     end
   end
